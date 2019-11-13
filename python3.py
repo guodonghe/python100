8aa2d4a91b0a4121e7004b6c7a9698ed5cab52ef
guo <1003993630@qq.com>
2019-11-13 18:54:29 +0800

    增加水仙数 纪念

--- python3.py
+++ python3.py
@@ -1,5 +1,14 @@
 # 乘法口诀
+'''
 for i in range(1,10):
     for j in range(1,i+1):
         print("{} * {} = {}".format(j,i,i*j),end=" ")
     print (" ")
+'''
+# 水仙花数
+for i in range(100,1000):
+    a = int(i/100)
+    b = int(i%100/10)
+    c = int(i%10)
+    if pow(a,3) + pow(b,3) + pow(c,3) == i:
+        print (i)
